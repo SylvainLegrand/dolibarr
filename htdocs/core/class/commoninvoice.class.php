002c43b091402a175ecec004c95792d7efb41342
Razafimanantsoa Tojonirina Tonny <128148191+RzTonny@users.noreply.github.com>
2025-10-16 16:52:07 +0300

    Update commoninvoice.class.php

--- htdocs/core/class/commoninvoice.class.php
+++ htdocs/core/class/commoninvoice.class.php
@@ -755,8 +755,40 @@ abstract class CommonInvoice extends CommonObject
 		if (!getDolGlobalString('INVOICE_CAN_ALWAYS_BE_REMOVED') && $this->getSommePaiement() > 0) {
 			return -4;
 		}
+		// test if there is a prelevement demand for this invoice and if it is refuse to delete.
+		if ($this instanceof FactureFournisseur) {
+			$hasprelevement = $this->hasPrelevementDemande();
+			if ($hasprelevement < 0) {
+				return $hasprelevement;
+			}
+		}
 
 		return 2;
+	}
+
+	/**
+	 *	Return if an invoice has a prelevement demand
+	 *
+	 *	@return		int					if prelevement demand found, 0=no demand, 1=yes
+	 */
+	public function hasPrelevementDemande()
+	{
+		if ($this instanceof FactureFournisseur) {
+			$sql = "SELECT fk_prelevement_bons, traite";
+			$sql .= " FROM ".$this->db->prefix(). "prelevement_demande";
+			$sql .= " WHERE fk_facture_fourn = ". ((int) $this->id);
+			$resql = $this->db->query($sql);
+			if ($resql) {
+				$obj = $this->db->fetch_object($resql);
+				if ($obj) {
+					if ($obj->traite == 0) {
+						return 1;
+					}
+					return (int) (-5 . $obj->fk_prelevement_bons);
+				}
+			}
+		}
+		return 0;
 	}
 
 	/**
