6b17c9c1989a839ec2ab57acf38a81b2cfcd2442
Razafimanantsoa Tojonirina Tonny <128148191+RzTonny@users.noreply.github.com>
2025-10-17 10:58:56 +0300

    Update list.php

--- htdocs/fourn/facture/list.php
+++ htdocs/fourn/facture/list.php
@@ -1752,7 +1752,7 @@ while ($i < $imaxinloop) {
 	} else {
 		// Show line of result
 		$j = 0;
-		print '<tr data-rowid="'.$object->id.'" class="oddeven '.((getDolGlobalInt('MAIN_FINISHED_LINES_OPACITY') == 1 && $obj->fk_statut > 1) ? 'opacitymedium' : '').'">';
+		print '<tr data-rowid="'.$object->id.'" class="oddeven '.((getDolGlobalInt('MAIN_FINISHED_LINES_OPACITY') == 1 && $obj->fk_statut > 1) ? 'opacitymedium' : '').'">'; // removing 'row-with-select' hides mass actions
 
 		// Action column
 		if (getDolGlobalString('MAIN_CHECKBOX_LEFT_COLUMN')) {
